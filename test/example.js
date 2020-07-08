ee96c81440f8e0fb3d056e4e9ee995c66d12409d
Iago S <iagosr@pm.me>
2020-07-07 21:39:09 -0300

    chore: update dependencies

--- test/example.js
+++ test/example.js
@@ -4,9 +4,11 @@ import { expect } from 'chai'
 import { shallow } from 'enzyme'
 import App from '../src/App'
 
+React.Suspense = ({ children }) => children
+
 describe('<App />', () => {
   it('call', () => {
     const wrapper = shallow(<App />)
-    expect(wrapper.contains(<main>A simple app</main>)).to.be.equal(true)
+    expect(wrapper.contains(<main>Home page</main>)).to.be.equal(true)
   })
 })
